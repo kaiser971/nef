54e149e383046f690d743af927b8ac881cfd0f30
Bathia <jbathia@sqli.com>
2018-11-22 17:06:21 +0100

    Mis en place Section Contact et component form + rajout slick js dans le dossier src

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 new fullpage('#fullpage', {
-    sectionsColor: ['yellow', '#f1F1F1', '#EFEFEF', '#EFEFEF'],
-  });
+    sectionsColor: ['#EFEFEF', '#EFEFEF', '#EFEFEF', '#EFEFEF', '#EFEFEF'],
+});
+
+$('.gallery').slick({
+    adaptiveHeight: false,
+    nextArrow: '<button type="button" class="slick-next"><svg width="60px" height="80px" viewBox="0 0 50 80" xml:space="preserve"><polyline fill="none" stroke="#a0a0a0" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" points="0.375,0.375 45.63,38.087 0.375,75.8"/></svg></button>',
+    prevArrow: '<button type="button" class="slick-prev"><svg width="60px" height="80px" viewBox="0 0 50 80" xml:space="preserve"><polyline fill="none" stroke="#a0a0a0" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" points="45.63,75.8 0.375,38.087 45.63,0.375"/></svg></button>',
+});
